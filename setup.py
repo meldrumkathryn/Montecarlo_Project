93fc0301f8522610ff55a76f06142c109168de0f
meldrumkathryn <97808270+meldrumkathryn@users.noreply.github.com>
2022-07-12 18:38:27 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='monte',
+      version='0.1',
+      description='dice tools',
+      url='https://github.com/meldrumkathryn/DS5100_Final/tree/main/monte',
+      author='Kate Meldrum',
+      author_email='kmm4ap@gmail.com',
+      license='MIT',
+      packages=find_packages())
+
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
